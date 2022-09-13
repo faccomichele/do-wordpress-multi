362c89fc8f1fd229e28f1447d3fc30a48fac2f3b
faccomichele <faccomichele@gmail.com>
2022-09-13 21:21:00 +0300

    ansible first fix

--- terraform/maint.tf
+++ terraform/maint.tf
@@ -18,9 +18,17 @@ resource "digitalocean_droplet" "wordpress" {
   size   = "s-1vcpu-512mb-10gb"
   ssh_keys = [ "e9:fb:ec:94:a8:b4:d8:be:81:f1:14:49:4a:f4:77:92" ]
   user_data = <<USERDATA
+#!/bin/bash
+
 WORKING_DIR=/tmp/user-data-exec
 mkdir -p $WORKING_DIR
-apt-get update && apt-get upgrade -y && apt-get install -y git ansible
+
+apt install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get upgrade -y
+apt-get install -y git ansible
+
 cd $WORKING_DIR
 git clone https://github.com/faccomichele/do-wordpress-multi.git
 cd do-wordpress-multi/ansible
